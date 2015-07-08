d560cdb6c30bd6e5c96a21014f5974aee1e2c5c6
Gileno Filho <gascf.cin@gmail.com>
2015-07-08 13:43:38 -0300

    ajustes

--- ifacademy/settings.py
+++ ifacademy/settings.py
@@ -100,6 +100,3 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = 'staticfiles'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
--- ifacademy/urls.py
+++ ifacademy/urls.py
@@ -2,10 +2,12 @@
 
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(
         r'^$', 'apps.core.views.index', name='index'
     ),
     url(r'^admin/', include(admin.site.urls)),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
