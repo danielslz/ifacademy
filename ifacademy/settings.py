b61a2e8a860bdc37322554e81f692f6af69af3f7
Daniel Lima <danielslz@gmail.com>
2015-07-09 08:45:39 -0300

    Atualizado código para versão do Gileno

--- ifacademy/settings.py
+++ ifacademy/settings.py
@@ -30,7 +30,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'apps.core',
+    'apps.comum',
+    'apps.cursos',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -74,7 +75,7 @@ DATABASES = {'default':  dj_database_url.config()}
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
@@ -92,6 +93,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = 'staticfiles'
 
 try:
-    .local_settings import *
+    from .local_settings import *
 except ImportError:
     pass
--- ifacademy/urls.py
+++ ifacademy/urls.py
@@ -6,8 +6,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(
-        r'^$', 'apps.core.views.index', name='index'
-    ),
+    url(r'^', include('apps.comum.urls')),
+    url(r'^cursos/', include('apps.cursos.urls')),
     url(r'^admin/', include(admin.site.urls)),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(
+    settings.STATIC_URL,
+    document_root=settings.STATIC_ROOT
+)
