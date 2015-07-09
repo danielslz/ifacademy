6ac1b1a8846cbdcc3b7c9633e21b4075abc0be3b
Daniel Lima <danielslz@gmail.com>
2015-07-09 09:30:05 -0300

    Adicionada a exibição das disciplinas

--- apps/cursos/urls.py
+++ apps/cursos/urls.py
@@ -1,15 +1,13 @@
 # coding=utf-8
 
-from django.conf.urls import include, url
+from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
-    url(
-        r'^$', views.cursos, name='cursos'
-    ),
-    url(
-        r'^(?P<pk>\d+)/$', views.curso_detalhes, 
-        name='curso_detalhes'
-    ),
+    url(r'^$', views.cursos, name='cursos'),
+    url(r'^(?P<curso_id>\d+)/$', views.curso_detalhes, name='curso_detalhes'),
+    url(r'^disciplinas/$', views.disciplinas, name='disciplinas'),
+    url(r'^disciplinas/(?P<disciplina_id>\d+)/$', views.disciplina_detalhes,
+        name='disciplina_detalhes'),
 ]
--- apps/cursos/views.py
+++ apps/cursos/views.py
@@ -1,22 +1,30 @@
 # coding=utf-8
 
 from django.shortcuts import render
-from .models import Curso
+from .models import Curso, Disciplina
 
 
 def cursos(request):
     contexto = {}
     contexto['cursos'] = Curso.objects.all()
-    return render(
-        request, 'cursos/cursos.html', contexto
-    )
+    return render(request, 'cursos/cursos.html', contexto)
 
 
-def curso_detalhes(request, pk):
-    curso = Curso.objects.get(pk=pk)
+def curso_detalhes(request, curso_id):
+    curso = Curso.objects.get(pk=curso_id)
     contexto = {
         'curso': curso
     }
-    return render(
-        request, 'cursos/curso_detalhes.html', contexto
-    )
+    return render(request, 'cursos/curso_detalhes.html', contexto)
+
+
+def disciplinas(request):
+    contexto = {}
+    contexto['disciplinas'] = Disciplina.objects.all()
+    return render(request, 'cursos/disciplinas.html', contexto)
+
+
+def disciplina_detalhes(request, disciplina_id):
+    contexto = {}
+    contexto['disciplina'] = Disciplina.objects.get(pk=disciplina_id)
+    return render(request, 'cursos/disciplina_detalhes.html', contexto)
