b3b7087210e35f38520e46efd2d0b3064b566168
Daniel Lima <danielslz@gmail.com>
2015-07-10 10:12:59 -0300

    Adicionado os teste da view suporte.

--- apps/comum/tests.py
+++ apps/comum/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+
+class SuporteViewTestCase(TestCase):
+    def setUp(self):
+        self.url = reverse('suporte')
+
+    def test_acesso_suporte_sem_login(self):
+        cliente = Client()
+        resposta = cliente.get(self.url)
+        self.assertEquals(resposta.status_code, 200)
+
+    def test_suporte_template_usado(self):
+        cliente = Client()
+        respota = cliente.get(self.url)
+        self.assertTemplateUsed(respota, 'suporte.html')
